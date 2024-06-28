7305cad045db410587af3ef5b3fcf41fd5e101f9
herve <herve.bakongo@hologram.cd>
2024-06-28 06:25:42 +0100

    initialisation projet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyComponent } from './company/company.component';
+import { ProductComponent } from './product/product.component';
+import { AddproductComponent } from './addproduct/addproduct.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'company',
+    component: CompanyComponent,
+  },
+  {
+    path: '',
+    component: CompanyComponent,
+  },
+  {
+    path: 'product',
+    component: ProductComponent,
+    children:[
+      {
+        path: 'create',
+        component: AddproductComponent
+      },
+      {
+        path: 'edit/:id',
+        component: AddproductComponent
+      }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductComponent } from './product/product.component';
+import { PopupComponent } from './popup/popup.component';
+import { CompanyComponent } from './company/company.component';
+import { AddproductComponent } from './addproduct/addproduct.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    PopupComponent,
+    CompanyComponent,
+    AddproductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
